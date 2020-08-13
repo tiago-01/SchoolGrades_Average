910daca54ad6b245a273d8e3cedec5362c6622c2
tiago-01 <67785681+tiago-01@users.noreply.github.com>
2020-08-13 17:37:33 +0100

    clean code

--- schoolgrades_avg.cpp
+++ schoolgrades_avg.cpp
@@ -17,34 +17,34 @@ void checkifexists(struct aluno *a);
 int checkifisvalid(int n);
 
 struct student{
-	int n;
-    float g1;
-    float g2;
-    float g3;
-    float avg;
+	int number;
+    float grade1;
+    float grade2;
+    float grade3;
+    float average;
 };
 
 struct schoolclass{
 	struct student students[5];
-    float avg;
+    float average;
 };
 
 void checkifexists(struct student *a){
 	int i=0;
-	int n=0;
+	int number=0;
 	here:
-	scanf("%i",&n);
+	scanf("%i",&number);
 	for (i=0;i<5;i++){
-		if (a[i].n==n){
+		if (a[i].number==number){
 			printf("Existing number\nWrite the right one: ");
 			goto here;
 		}
-		a[i].n=n;
+		a[i].number=number;
 	}
 }
 
-int checkifisvalid(int n){  
-	if (n>=0 && n<=20){
+int checkifisvalid(int number){  
+	if (number >= 0 && number <= 20){
 		 return true;
 	    } else {
 	    	printf("Grade not valid\n");
@@ -58,16 +58,18 @@ void class_average(struct schoolclass *c,struct student *students){
 	 float grades = 0;
 	 for(i=0;i<5;i++){
 	   grades = 0;
-	   grades = students[i].g1 + students[i].g2 + students[i].g3;
+	   grades = students[i].grade1 + students[i].grade2 + students[i].grade3;
 	   sumgrades = sumgrades + grades;	
 	 }
-	 c->avg = sumgrades / 15;
-	 printf("\nClass average: %0.2f\n",c->avg);
+	 c->average = sumgrades / 15;
+	 printf("\nClass average: %0.2f\n",c->average);
 	 
 }
 
-void grades(struct student *a){
-	int i=0; int n=0; float g1=0; float g2=0; float g3=0;
+void getgrades(struct student *a){
+	int i = 0; 
+	int number = 0; 
+	float grade1 = 0; float grade2 = 0; float grade3 = 0;
     
 	for (i=0;i<5;i++){
 		printf("Student number':");
@@ -75,36 +77,36 @@ void grades(struct student *a){
 		
 		g1:	
 		printf("Grade 1 = ");
-		scanf("%f",&g1);
-		if (checkifisvalid(g1)==1){
-			a[i].g1=g1;
+		scanf("%f",&grade1);
+		if (checkifisvalid(grade1)==1){
+			a[i].grade1=grade1;
 		} else {
 			goto g1;
 		}
 		
 		g2:
 		printf("Grade 2 = ");
-		scanf("%f",&g2);
-	    if (checkifisvalid(g2)==1){
-			a[i].g2=g2;
+		scanf("%f",&grade2);
+	    if (checkifisvalid(grade2)==1){
+			a[i].grade2=grade2;
 		} else {
 			goto g2;
 		}
 		
 		g3:
 		printf("Grade 3 = ");
-		scanf("%f",&g3);
-	    if (checkifisvalid(g3)==1){
-			a[i].g3=g3;
+		scanf("%f",&grade3);
+	    if (checkifisvalid(grade3)==1){
+			a[i].grade3=grade3;
 		} else {
 			goto g3;
 		}
 		
-		a[i].avg=(a[i].g1 + a[i].g2 + a[i].g3 ) / 3;
-		if (a[i].avg >=10){
-			printf ("The student number %i passed with an average of: %0.2f\n",a[i].n,a[i].avg);
+		a[i].average=(a[i].grade1 + a[i].grade2 + a[i].grade3 ) / 3;
+		if (a[i].average >=10){
+			printf ("The student number %i passed with an average of: %0.2f\n",a[i].number,a[i].average);
 		} else {
-			printf ("he student number %i did not pass. Average: %0.2f\n",a[i].n,a[i].avg);
+			printf ("he student number %i did not pass. Average: %0.2f\n",a[i].number,a[i].average);
 		}	
 	}
 }
@@ -112,7 +114,7 @@ void grades(struct student *a){
 
 int main(){
 	struct student a;
-	grades(&a);
+	getgrades(&a);
 	struct schoolclass b;
 	class_average(&b,&a);
 	return 0;
